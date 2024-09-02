86fd6525eedf246a1e32e48c3102409ea47c04c3
Vivek Kumar <k99vivek@gmail.com>
2024-09-02 01:34:34 -0700

    Remove extraneous code in lspconfig.lua

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -3,17 +3,14 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
-local servers = { "clangd", "pyright" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
-end
+-- configuring single server, example: typescript
+-- lspconfig.tsserver.setup {
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }
 
 lspconfig.pyright.setup({
   on_attach = nvlsp.on_attach,
@@ -29,9 +26,3 @@ lspconfig.clangd.setup({
   filetypes = { "cpp" },
 })
 
--- configuring single server, example: typescript
--- lspconfig.tsserver.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
