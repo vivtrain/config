01158014d743c26277d20f3a17f7ef028abc883a
Vivek Kumar <k99vivek@gmail.com>
2024-09-12 14:05:43 -0700

    Fix newlines in nvim system clipboard

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -8,8 +8,8 @@ vim.g.clipboard = {
             ["*"] = "clip.exe",
         },
         paste = {
-            ["+"] = "powershell.exe Get-Clipboard",
-            ["*"] = "powershell.exe Get-Clipboard",
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
         },
         cache_enabled = true,
     }
